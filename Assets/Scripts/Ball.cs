bb987244e5ad0c759ffd73e1ea02589f99423ac0
Bogay <pojay11523@gmail.com>
2019-02-15 03:29:54 +0800

    remove unused code

--- Assets/Scripts/Ball.cs
+++ Assets/Scripts/Ball.cs
@@ -4,63 +4,18 @@ using UnityEngine;
 
 public class Ball : MonoBehaviour
 {
-	public float force;
 	public float speed;
-
-	private float delta = 20;
-	private Vector2[] axesv = { Vector2.right, Vector2.up, Vector2.left, Vector2.down };
-	private float[] axes = { 0, 90, 180, -90 };
+	public float forceScalor;
 
 	private Rigidbody2D rb2d;
-	private float last;
-	private float gap = 0.2f;
+	private Transform self;
 
 	void Start()
 	{
-		last = Time.time;
+		self = transform;
 		rb2d = GetComponent<Rigidbody2D>();
-		rb2d.AddForce(Vector2.down * force);
+		rb2d.velocity = Vector2.down * speed;
 	}
-
-	
-	// private void OnCollisionExit2D(Collision2D other)
-	// {
-	// 	if(Time.time - last < gap) return;
-	// 	Debug.Log(other.gameObject.tag);
-	// 	float angle;
-	// 	float d, r;
-		
-	// 	for(int i=0 ; i<4 ; i++)
-	// 	{
-	// 		angle = Vector2.Angle(axesv[i], rb2d.velocity);
-	// 		d = axes[i] - angle;
-	// 		if(Mathf.Abs(angle) < delta)
-	// 		{
-	// 			r = (d + (delta + 5) * Mathf.Sign(d)) * Mathf.Sign(angle);
-	// 			Debug.Log("roatation: " + r);
-	// 			rb2d.velocity = Quaternion.Euler(0, 0, r) * rb2d.velocity;
-	// 			break;
-	// 		}
-	// 	}
-		
-	// 	angle = Vector2.Angle(Vector2.right, rb2d.velocity);
-	// 	Debug.Log(angle);
-		
-	// 	rb2d.velocity = rb2d.velocity.normalized * speed;
-	// 	last = Time.time;
-	// }
-	
-
-	// void OnCollisionExit2D(Collision2D collision)
-    // {
-    //     float angle = Mathf.Rad2Deg * Mathf.Atan2(rb2d.velocity.y, rb2d.velocity.x);
-    //     float a = Mathf.Repeat(angle + 180f, 90f);
-    //     float b = Mathf.Clamp(a, 20, 70);
-	// 	Debug.Log(System.String.Format("angle, a, b = [{0}, {1}, {2}]", angle, a, b));
-    //     angle = Mathf.Deg2Rad * (angle - a + b);
-	// 	Debug.Log(System.String.Format("Fixed angle: {0}", angle));
-    //     rb2d.velocity = speed * new Vector2(Mathf.Cos(angle), Mathf.Sin(angle));
-    // }
 
 	private void OnCollisionExit2D(Collision2D other)
 	{
@@ -69,5 +24,18 @@ public class Ball : MonoBehaviour
 		float b = Mathf.Clamp(a, 20, 70);
 		angle = Mathf.Deg2Rad * (angle - a + b);
 		rb2d.velocity = speed * new Vector2(Mathf.Cos(angle), Mathf.Sin(angle));
+	}
+
+	private void OnCollisionEnter2D(Collision2D other)
+	{
+		if(other.gameObject.tag == "GameController")
+		{
+			//Debug.Log(System.String.Format("{0}, {1}", other.contacts[0].point, self.position));
+			float temp = self.position.x - other.transform.position.x;
+			if(Mathf.Abs(temp) > 1)
+				temp = temp * 1 / Mathf.Abs(temp);
+			print(temp);
+			rb2d.AddForce(Vector2.right * (temp * forceScalor));
+		}
 	}
 }
--- Assets/Scripts/PlayerControl.cs
+++ Assets/Scripts/PlayerControl.cs
@@ -5,17 +5,14 @@ using UnityEngine;
 public class PlayerControl : MonoBehaviour
 {
 	public float speed;
-	public float forceScalor;
 
 	private Transform self;
-	private Rigidbody2D rb2d;
 	private Vector3 speedV;
 
 	// Use this for initialization
 	void Start()
 	{
 		self = transform;
-		rb2d = GetComponent<Rigidbody2D>();
 		speedV = Vector3.right * speed;
 	}
 
@@ -24,15 +21,5 @@ public class PlayerControl : MonoBehaviour
 	{
 		if(Input.GetKey(KeyCode.LeftArrow)) self.position -= speedV * Time.deltaTime;
 		else if(Input.GetKey(KeyCode.RightArrow)) self.position += speedV * Time.deltaTime;
-	}
-
-	private void OnCollisionEnter2D(Collision2D other)
-	{
-		if(other.gameObject.tag == "Player")
-		{
-			//Debug.Log(System.String.Format("{0}, {1}", other.contacts[0].point, self.position));
-			float temp = other.contacts[0].point.x - self.position.x;
-			other.gameObject.GetComponent<Rigidbody2D>().AddForce(Vector2.right * (temp * forceScalor));
-		}
 	}
 }
