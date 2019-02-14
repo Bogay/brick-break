a5e40a6b8c56c0e46674677b831f1147c0e7f425
Bogay <pojay11523@gmail.com>
2019-02-15 03:30:19 +0800

    add start scene

---
+++ Assets/Scripts/AnyKeyEvent.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class AnyKeyEvent : MonoBehaviour
+{
+	public void listenForStart()
+	{
+		StartCoroutine("listenAnyKey");
+	}
+
+	private IEnumerator listenAnyKey()
+	{
+		yield return new WaitUntil(() => Input.anyKey);
+		SceneManager.LoadScene("Stage");
+	}
+}
---
+++ Assets/Scripts/TitleEvents.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class TitleEvents : MonoBehaviour
+{
+	public void showAnyKey()
+	{
+		GameObject anyKey = GameObject.Find("AnyKey");
+		Animation anim = anyKey.GetComponent<Animation>();
+		anim.Play();
+	}
+}
